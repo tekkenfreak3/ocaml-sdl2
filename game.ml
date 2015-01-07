ac214ff3145ef6b89930fe9f7b35ea644b68aa71
Daniel Wilkins <tekk@parlementum.net>
2015-01-07 02:12:59 -0500

    Finished modularization. Added texture_exists to Render module to allow Image to continue to return options. todo later: optional src and dst rects for Render.copy which defaults to full texture, full surface.

--- game.ml
+++ game.ml
@@ -1,24 +1,23 @@
 (* Copyright (C) 2014 Daniel Wilkins  *)
 open Ctypes;;
-  
+
 let rec main () = 
-  ignore (Sdl2.init Sdl2.init_everything);
+  ignore (Sdl2.Init.init Sdl2.Init.init_everything);
   let win = match (Sdl2.Window.make "foobar?" 640 480 640 480 0) with
     |Some w -> w in
-  let renderer = match (Sdl2.create_renderer win 2) with
+  let renderer = match (Sdl2.Render.make win 2) with
     | Some r -> r in
-  Printf.eprintf "%B\n%!" (renderer = Ctypes.null);
   let img = match Sdl2.Image.load renderer "resources/images/bg.png" with
     | Some i -> i
   in
 
   let rec iloop () =
-    let res = Sdl2.render_copy_f renderer img {x=0;y=0;w=3000;h=3000} {x=0;y=0;w=640;h=480}  in
-    Sdl2.render_present renderer;
-    let ev = Ctypes.make Sdl2.event in
-    ignore (Sdl2.poll_event (Ctypes.addr ev));
+    let res = Sdl2.Render.copy {x=0;y=0;w=3000;h=3000} {x=0;y=0;w=640;h=480} renderer img in
+    Sdl2.Render.present renderer;
+    let ev = Ctypes.make Sdl2.Event.t in
+    ignore (Sdl2.Event.poll_event (Ctypes.addr ev));
 
-    if (getf ev Sdl2.event_type) <> Sdl2.quit_event
+    if (getf ev Sdl2.Event.etype) <> Sdl2.Event.quit_event
     then
       iloop () in
   iloop ();
--- sdl2.ml
+++ sdl2.ml
@@ -2,20 +2,13 @@
 open Ctypes;;
 open Foreign;;
 open Unsigned;;
-  
-
-type texture = unit ptr;;
-let texture : texture typ = ptr void;;
-
-type renderer = unit ptr;;
-let renderer : renderer typ = ptr void;;
-
+open Core.Result;;
 module Rect : sig
-  type ocaml_rect = {x: int; y: int; w: int; h: int};;
+  type rect = {x: int; y: int; w: int; h: int};;
   type t;;
-  val t : ocaml_rect typ;;
+  val t : rect typ;;
 end = struct
-  type ocaml_rect = {x: int; y: int; w: int; h: int}
+  type rect = {x: int; y: int; w: int; h: int}
 
   type sdl_rect;;
   let sdl_rect : sdl_rect structure typ = structure "SDL_Rect";;
@@ -25,37 +18,59 @@ end = struct
   let h = field sdl_rect "h" int;;
   seal sdl_rect;;
 
-  let sdl_rect_of_ocaml_rect rect =
-    let ret = make sdl_rect in
-    setf ret x rect.x;
-    setf ret y rect.y;
-    setf ret w rect.w;
-    setf ret h rect.h;
-    (addr ret);;
-  let ocaml_rect_of_sdl_rect r =
+  let sdl_rect_of_rect rect =
+    if rect.w <> 0
+    then
+      begin
+	let ret = make sdl_rect in
+	setf ret x rect.x;
+	setf ret y rect.y;
+	setf ret w rect.w;
+	setf ret h rect.h;
+	(addr ret)
+      end	  
+    else
+      (from_voidp sdl_rect null);;
+	  
+  let rect_of_sdl_rect r =
     let rect = !@ r in
     {x = (getf rect x);y = (getf rect y);w = (getf rect w);h = (getf rect h)};;
   type t;;
-  let t = view ~read:ocaml_rect_of_sdl_rect ~write:sdl_rect_of_ocaml_rect (ptr sdl_rect);;
+  let t = view ~read:rect_of_sdl_rect ~write:sdl_rect_of_rect (ptr sdl_rect);;
 end
 
-let init_timer = 1;;
-let init_audio = 16;;
-let init_video = 32;;
-let init_joystick = 512;;
-let init_events = 0x00004000;;
-let init_everything = init_timer lor init_audio lor init_video lor init_joystick lor init_events;;
+module Error = struct
+  let get_error = foreign "SDL_GetError" (void @-> returning string);;
+end
 
-let init_f = foreign "SDL_Init" (int @-> returning int);;
-let get_error = foreign "SDL_GetError" (void @-> returning string);;
+module Init : sig
+  val init_timer : int;;
+  val init_audio : int;;
+  val init_video : int;;
+  val init_joystick : int;;
+  val init_events : int;;
+  val init_everything : int;;
 
-let init flags =
-  if init_f flags = 1
-  then begin
-      Printf.eprintf "SDL failed to initialize, this may help: %s\n" (get_error ());
-      raise Exit;
-    end;;
+  val init : int -> unit;;
+end = struct
+  let init_timer = 1;;
+  let init_audio = 16;;
+  let init_video = 32;;
+  let init_joystick = 512;;
+  let init_events = 0x00004000;;
+  let init_everything = init_timer lor init_audio lor init_video lor init_joystick lor init_events;;
 
+  let init_f = foreign "SDL_Init" (int @-> returning int);;
+
+
+  let init flags =
+    if init_f flags = 1
+    then begin
+	Printf.eprintf "SDL failed to initialize, this may help: %s\n" (Error.get_error ());
+	raise Exit;
+      end;;
+end
+		
 module Window : sig
   type t
   val t : t typ;;
@@ -74,68 +89,94 @@ end = struct
 end
 	
 let delay = foreign "SDL_Delay" (int @-> returning void);;
-  
-let create_renderer_f = foreign "SDL_CreateRenderer" (Window.t @-> int @-> int @-> returning renderer);;
 
-  
-let create_renderer window flags =
-  let rend = create_renderer_f window ~-1 flags in (* ~-1 is to avoid confusing the parser, it just means -1*)
-  if rend <> null
-  then
-    Some rend
-  else
-    None;;
 
-let render_copy_f = foreign "SDL_RenderCopy" (renderer @-> texture @-> Rect.t @-> Rect.t @-> returning int);;
-let render_present = foreign "SDL_RenderPresent" (renderer @-> returning void);;
+module Render : sig
+  type t
+  val t : t typ
+  type texture
+  val texture : texture typ
+  val texture_exists : texture -> bool
+  val make : Window.t -> int -> t option
+  val copy : Rect.rect -> Rect.rect -> t -> texture -> (unit, string) Core.Result.t
+  val present : t -> unit
+end = struct
+  type t = unit ptr;;
+  let t : t typ = ptr void;;
+  type texture = unit ptr;;
+  let texture : texture typ = ptr void;;
+
+  let texture_exists tex =
+    tex <> null;;
+  let create_renderer_f = foreign "SDL_CreateRenderer" (Window.t @-> int @-> int @-> returning t);;
+  
+  let make window flags =
+    let rend = create_renderer_f window ~-1 flags in (* ~-1 is to avoid confusing the parser, it just means -1*)
+    if rend <> null
+    then
+      Some rend
+    else
+      None;;
+
+  let copy_f = foreign "SDL_RenderCopy" (t @-> texture @-> Rect.t @-> Rect.t @-> returning int);;
+  let copy src dest renderer texture =
+    let ret = copy_f renderer texture src dest in
+    if ret = 0 then
+      Ok ()
+    else
+      Error (Error.get_error ());;
+    
+  let present = foreign "SDL_RenderPresent" (t @-> returning void);;
+end
+
 let quit = foreign "SDL_Quit" (void @-> returning void);;
 
-let quit_event = 0x100;;
-let keydown_event = 0x300;;
-let keyup_event = 0x301;;
-type window_event;;
-let window_event : window_event structure typ = structure "SDL_WindowEvent";;
-let window_event_type = field window_event "type" int;;
-let window_event_timestamp = field window_event "timestamp" int;;
-let window_event_windowID = field window_event "type" int;;
-let window_event_event = field window_event "type" int;;
-let window_event_pad1 = field window_event "type" uint8_t;;
-let window_event_pad2 = field window_event "type" uint8_t;;
-let window_event_pad3 = field window_event "type" uint8_t;;
-let window_event_data1 = field window_event "type" int;;
-let window_event_data2 = field window_event "type" int;;
-let window_event_data3 = field window_event "type" int;;
-let window_event_data4 = field window_event "type" int;;
-let window_event_data5 = field window_event "type" int;;
-let window_event_data6 = field window_event "type" int;;
-let window_event_data7 = field window_event "type" int;;
-let window_event_data8 = field window_event "type" int;;
-let window_event_data9 = field window_event "type" int;;
-let window_event_data10 = field window_event "type" int;;
-let window_event_data11 = field window_event "type" int;;
-let window_event_data12 = field window_event "type" int;;
-let window_event_data13 = field window_event "type" int;;
-let window_event_data14 = field window_event "type" int;;
-  
-  seal window_event;;
+module Event = struct
+  let quit_event = 0x100;;
+  let keydown_event = 0x300;;
+  let keyup_event = 0x301;;
+  type window;;
+  let window : window structure typ = structure "SDL_WindowEvent";;
+  let wtype = field window "type" int;;
+  let timestamp = field window "timestamp" int;;
+  let windowID = field window "type" int;;
+  let win = field window "type" int;;
+  let data1 = field window "type" int;;
+  let data2 = field window "type" int;;
+  let window_data3 = field window "type" int;;
+  let window_data4 = field window "type" int;;
+  let window_data5 = field window "type" int;;
+  let window_data6 = field window "type" int;;
+  let window_data7 = field window "type" int;;
+  let window_data8 = field window "type" int;;
+  let window_data9 = field window "type" int;;
+  let window_data10 = field window "type" int;;
+  let window_data11 = field window "type" int;;
+  let window_data12 = field window "type" int;;
+  let window_data13 = field window "type" int;;
+  let window_data14 = field window "type" int;;
     
-type event;;
-let event : event union typ = union "SDL_Event";;
-let event_type = field event "type" int;;
-let event_1 = field event "window" window_event;;
-seal event;;
+    seal window;;
+      
+    type t;;
+    let t: t union typ = union "SDL_Event";;
+    let etype = field t "type" int;;
+    let window = field t "window" window;;
+      seal t;;
 
-let poll_event = foreign "SDL_PollEvent" (ptr event @-> returning int);;
+      let poll_event = foreign "SDL_PollEvent" (ptr t @-> returning int);;
+end
+		 
 module Image : sig
   val quit : unit -> unit -> unit;;
-  val load_f : renderer -> string -> texture;;
-  val load : renderer -> string -> texture option;;
+  val load_f : Render.t -> string -> Render.texture;;
+  val load : Render.t -> string -> Render.texture option;;
 end = struct
   let quit () = foreign "IMG_Quit" (void @-> returning void);;
-  let load_f = foreign "IMG_LoadTexture" (renderer @-> string @-> returning texture);;
+  let load_f = foreign "IMG_LoadTexture" (Render.t @-> string @-> returning Render.texture);;
   let load renderer texname =
     let tex = load_f renderer texname in
-    if tex <> null
+    if Render.texture_exists tex
     then
       Some tex
     else
